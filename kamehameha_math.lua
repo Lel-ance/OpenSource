7612637d3eab4e75a469c5aab867ade878e9a534
Lelance <55248223+Lel-ance@users.noreply.github.com>
2020-11-17 18:11:31 +0000

    Update kamehameha_math.lua

--- kamehameha_math.lua
+++ kamehameha_math.lua
@@ -2,7 +2,7 @@
 			local core = beam.Core
 			local maxDistBonus = 700
 			local distFromCore = math.clamp((target.Character.HumanoidRootPart.Position - core.Position).magnitude, 1, maxDistBonus)
-            local PLAYER_LEVEL
+           		local PLAYER_LEVEL
 			local lvlMult = math.clamp(PLAYER_LEVEL, 4, math.huge) or 4
 			local base = 80
 			return math.abs(base*lvlMult/2)+((maxDistBonus-distFromCore)*(PLAYER_LEVEL < 4 and PLAYER_LEVEL or lvlMult)*.8)
