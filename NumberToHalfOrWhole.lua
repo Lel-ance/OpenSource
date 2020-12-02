f0ccc04e1eddb44bca9f885d1dfe6adbca7ab8ec
Riyury <55248223+Lel-ance@users.noreply.github.com>
2020-12-02 16:43:23 +0000

    Update NumberToHalfOrWhole.lua

--- NumberToHalfOrWhole.lua
+++ NumberToHalfOrWhole.lua
@@ -1,11 +1,11 @@
-local function FillUpRating(Amnt)
-	local AmountOfFullRatings = Amnt%1 == 0 and math.ceil(Amnt) or math.ceil(Amnt) - 1
-	local AmountOfHalfRatings = Amnt%1 == 0 and 0 or 1
+local function FillUp(Amnt)
+	local AmountOfFull = Amnt%1 == 0 and math.ceil(Amnt) or math.ceil(Amnt) - 1
+	local AmountOfHalf = Amnt%1 == 0 and 0 or 1
 	
-	warn("INPUT: " .. tostring(Amnt) .. " Full Ratings: " .. AmountOfFullRatings .. " | Half Ratings: " .. AmountOfHalfRatings)
-  return AmountOfFullRatings, AmountOfHalfRatings
+	warn("INPUT: " .. tostring(Amnt) .. " Whole: " .. AmountOfFull .. " | Half: " .. AmountOfHalf)
+  return AmountOfFull, AmountOfHalf
 end
 
 -- Testing: 
 local TestWith = {1,1.5,1.75,2,2.34}
-table.foreach(TestWith, function(_, a) FillUpRating(a) end)
+table.foreach(TestWith, function(_, a) FillUp(a) end)
