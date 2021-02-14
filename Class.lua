07e656488ec12805653e84241da384a2b337173a
Riyury <55248223+Lel-ance@users.noreply.github.com>
2021-02-14 12:42:30 +0000

    Class.lua
    Make OOP (Object-Oriented-Programming) easy.

---
+++ Class.lua
@@ -0,0 +1,42 @@
+-- Lelance @ Sun. 14 Feb. 2021
+-- Script: Class function - make OOP easy.
+local function Class(preset: table)
+	local Proxy = newproxy(true)
+	local Meta = getmetatable(Proxy)
+	Meta.__len = function()
+		local class = {}
+		class.__index = class
+		setmetatable(type(preset[1]) == 'table' and preset[1] or {}, class)
+		function class:new(...)
+			local newClass = class
+			local func
+			if (typeof(preset[1]) == 'function') then
+				func = preset[1]
+			elseif (typeof(preset[2]) == 'function') then
+				func = preset[2]
+			else
+				func = function(self)
+					return self
+				end
+			end
+			return func(newClass, ...)
+		end
+		return class
+	end
+	return Proxy
+end
+
+-- Example usage:
+
+local ret = #Class {{}, function(self, passed)
+	self.pass = passed
+	warn(self.pass)
+	return self
+end}
+
+local new = ret:new 'yay'
+function new:cool()
+	return 'nice'
+end
+
+warn(new:cool())
