6a86740414b0c11ca15c7a6c557cf6d71a43c7e7
Riyury <55248223+Lel-ance@users.noreply.github.com>
2021-05-15 13:50:12 +0100

    RateLimitV2.lua
    Rewrite of RateLimit, new features, bug fixes, much faster.

---
+++ RateLimitV2.lua
@@ -0,0 +1,70 @@
+-- RateLimitV2
+-- Rewrite of V1, new "refreshWhenCalled" functionality, OOP based.
+
+local RateLimit = {
+	Class = {},
+	Nested = {}
+}
+
+RateLimit.Class.__index = RateLimit.Class
+
+local DEFAULTS = {
+	COOLDOWN = 5
+}
+
+local MESSAGES = {
+	ASSERTION_ERROR = ("RiyLimit: Invalid Parameter: %s")
+}
+
+function RateLimit:new(Name, Cooldown, refreshWhenCalled)
+	assert(typeof(Name) == 'string', MESSAGES.ASSERTION_ERROR:format(("[:new] 'Name' Expected string got %s"):format(typeof(Name))))
+	if not Cooldown then
+		Cooldown = DEFAULTS.COOLDOWN
+	end
+	assert(Cooldown, MESSAGES.ASSERTION_ERROR:format(("[:new] Expected some form of cooldown, not passed nor is a default set")))
+	assert(typeof(Cooldown) == 'number', MESSAGES.ASSERTION_ERROR:format(("[:new] 'Cooldown' Expected number got %s"):format(typeof(Cooldown))))
+	assert(not self.Nested[Name], MESSAGES.ASSERTION_ERROR:format(("[:new] %s is already hooked up in the rate limiter"):format(Name)))
+	local Created = setmetatable({
+		_name = Name,
+		_cooldown = Cooldown,
+		_refreshOnCall = refreshWhenCalled or false,
+		
+		_stored = setmetatable({}, {__mode = 'k'})
+	}, self.Class)
+	self.Nested[Name] = Created
+	return Created
+end
+
+function RateLimit:add(Player)
+	assert(self._name, MESSAGES.ASSERTION_ERROR:format(("[:add] Expected the method to be used on a RateLimit class, got direct call")))
+	assert(Player, MESSAGES.ASSERTION_ERROR:format(("[:add] Expected a player to be passed, got nil")))
+	
+	if not self._stored[Player] then
+		self._stored[Player] = 0
+		return false
+	end
+	local CompareTime, CurrentTime = self._stored[Player], tick()
+	local LimitNumber = self._cooldown
+
+	if (CurrentTime - CompareTime) <= LimitNumber then
+		if self._refreshOnCall then
+			self._stored[Player] = tick()
+		end
+		return true
+	else
+		self._stored[Player] = tick()
+	end
+	return false
+end
+
+function RateLimit:remove(Player)
+	assert(self._name, MESSAGES.ASSERTION_ERROR:format(("[:remove] Expected the method to be used on a RateLimit class, got direct call")))
+	assert(Player, MESSAGES.ASSERTION_ERROR:format(("[:remove] Expected a player to be passed, got nil")))
+	pcall(function() self._stored[Player] = nil  end)
+end
+
+function RateLimit:New(...) return self:new(...) end
+function RateLimit:Add(...) return self:add(...) end
+function RateLimit:Remove(...) return self:remove(...) end
+
+return RateLimit
